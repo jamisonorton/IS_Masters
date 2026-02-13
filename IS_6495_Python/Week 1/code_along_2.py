9bfb2214c99756667dea21cf76a533fbe1c3ecab
Jamison Orton <jorton4@byu.edu>
2026-02-13 10:10:02 -0700

    finished code along 2

---
+++ IS_6495_Python/Week 1/code_along_2.py
@@ -0,0 +1,17 @@
+x = 5
+x = x + 2
+print(x)
+
+x = 3.0 * x + 1.0
+print(x)
+
+interest_rate = 4
+interest_rate = x / interest_rate
+print(interest_rate)
+
+
+pay_rate = 12.00
+minutes = 1140
+hours = minutes / 60
+results = pay_rate * hours
+print(f"Pay result is $" + str(results))
