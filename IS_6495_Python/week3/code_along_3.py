c60c349dee25099e4371fd3340d9df5a1517533e
Jamison Orton <jorton4@byu.edu>
2026-02-25 10:10:25 -0700

    finished code_along 2 and 3

---
+++ IS_6495_Python/week3/code_along_3.py
@@ -0,0 +1,24 @@
+
+
+# apples = "apples"
+# for c in apples:
+#     print(c)
+
+# fruit = "oranges"
+# for letter in fruit:
+#     print(letter)
+# print("done")
+# print(fruit)
+
+total = 0
+for i in range(10000):
+    total += i
+
+print("Sum of all members from 0 to 10000",total)
+
+characters = ['Batman', 'Superman', 'Aquaman', 'Green Lantern', 'Wonder Woman']
+for item in characters:
+    print(item)
+
+characters.append('Flash')
+print(characters)
