b74e2249dc3988c59e9b1ee12f1e951efb0e6f3b
Jamison Orton <jorton4@byu.edu>
2026-03-06 17:42:46 -0700

    finished ice 1 and 2

--- IS_6495_Python/week4/ice1.py
+++ IS_6495_Python/week4/ice1.py
@@ -1,25 +1,36 @@
 # 4.1. Split Number List (2.5 points)
 # Use the string.split method create a list of numbers from the following string and then print the list to the screen.
-# '10 67 123 46 20 18 36 250'
+my_list = "10 67 123 46 20 18 36 250"
+split_list = my_list.split()
+print(split_list)
 
 
 # Do the same thing for this string:
-# '10,67,123,46,20,18,36,250'
+new_list = "10,67,123,46,20,18,36,250"
+new_split = new_list.split(",")
+print(new_split)
 
 
 # 4.2 Split Data into List (2.5 points)
 # Use the string.split method create a list of numbers from the following string and then sum up the numbers.  Print the sum to the screen.
-# '90,67,87,102,77,80'
+string_list = "90,67,87,102,77,80"
+num_split = [int(x) for x in string_list.split(",")]
+print(sum(num_split))
 
 
 # 4.3 Slice Lists (2.5 points)
 # Use the slicing syntax of lists to get the first 4 numbers in the following list and print out the results.
-# [1,2,3,4,5,6,7,8,9]
+list_a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+first_four = list_a[0:4]
+print(first_four)
 
 
 # 4.4 Slice Lists with Increment (2.5 points)
 # Use the slicing syntax of lists to get every other entry in the following list starting at the beginning and print the results.
-# ['a','b','c','d','e','f','g']
 
 # Sample output:
 # ['a', 'c', 'e', 'g']
+
+list_b = ["a", "b", "c", "d", "e", "f", "g"]
+sliced_list = list_b[0::2]
+print(sliced_list)
--- IS_6495_Python/week4/ice2.py
+++ IS_6495_Python/week4/ice2.py
@@ -1,14 +1,40 @@
 # 1.1 Create Dictionaries (2.5 points)
-# Use any names and birthdays you want to create a birthday dictionary that has four entries.  The name is the key and the value is the birth date.  Print each birth date by using the key to access each entry.
+# Use any names and birthdays you want to create a birthday dictionary that has four entries.  The name is the key and the value is the birth date.
+# Print each birth date by using the key to access each entry.
+
+birthday_dict = {
+    "Jamison": "August 12, 1994",
+    "Jane": "June 19, 1997",
+    "Theodore": "September 28, 2021",
+    "Tessa": "April 16, 2024",
+}
+for name in birthday_dict:
+    print(birthday_dict[name])
 
 
 # 1.2 Update Dictionaries (2.5 points)
 # Using the dictionary from above, update the last entry and change the birth date to 06/06/1980.
 
+birthday_dict.update({"Tessa": "06/06/1980"})
+# print(birthday_dict)
+
 
 # 1.3 Dictionary With Lists (2.5 points)
 # Create a dictionary of the seasons Fall, Spring and Summer where the name of the season is the key and the value is a list of the months in that season. Print the value of "Fall".
 
+seasons = {
+    "Fall": ["September", "October", "November"],
+    "Spring": ["March", "April", "May"],
+    "Summer": ["June", "July", "August"],
+}
+print(seasons["Fall"])
+
 
 # 1.4 Dictionary Merge (2.5 points)
-# Create the same dictionary as in exercise 3 but also create a second dictionary with only the season of Winter.  Use the dictionary.update method to merge the winter dictionary into the seasons dictionary.  Print the seasons dictionary.
+# Create the same dictionary as in exercise 3 but also create a second dictionary with only the season of Winter.
+# Use the dictionary.update method to merge the winter dictionary into the seasons dictionary.  Print the seasons dictionary.
+
+winter_season = {"Winter": ["December", "January", "February"]}
+
+seasons.update(winter_season)
+print(seasons)
