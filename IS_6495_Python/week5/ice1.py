451497bec4c0fbd076fdfcc39fc45f9bee71ec7e
Jamison Orton <jorton4@byu.edu>
2026-03-08 22:28:33 -0600

    week 5 template complete

---
+++ IS_6495_Python/week5/ice1.py
@@ -0,0 +1,13 @@
+#1.1. Hello World (again) (3 points)
+# Write a function that asks for the user’s name and prints “Hello, “ followed by their name.
+
+
+
+#1.2. Dog Years (3 points)
+# Write a function that asks for the age of the user’s dog. Print a string that states the dog’s age in dog years with a conversion rate of 1 human year to 7 dog years.
+
+
+
+#1.3. Purchase (3 points)
+# Write a function that asks for the user to enter a number of items they wish to purchase.
+
---
+++ IS_6495_Python/week5/ice2.py
@@ -0,0 +1,18 @@
+#2.1. Sum of numbers (3 points)
+# Write a function that takes a list of numbers and returns the sum of the numbers.
+
+
+
+#2.2. Number power (3 points)
+# Write a function that takes two integers and raises the first number to the power of the second number and returns the result.
+
+
+
+#2.3. Tax function (3 points)
+# Write a function that takes the price of the item as an argument and return the price calculated with a tax value of .07. The goal here is to convert the tax calculation into a reusable function. (Reflective from HW2, problem 2.7)
+
+
+
+#2.4. Average function (3 points)
+# Write a function that takes three arguments (numerical) and returns the average of the numbers entered.
+
