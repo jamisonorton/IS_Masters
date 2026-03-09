b5b78a02b78689591d64c7372fe72e6aac386247
Jamison Orton <jorton4@byu.edu>
2026-03-09 14:08:42 -0600

    finished code along 3 and debugger

---
+++ IS_6495_Python/week5/code_along_3.py
@@ -0,0 +1,70 @@
+# week 5
+# code along 3
+
+theBoard = {
+    "top-L": " ",
+    "top-M": " ",
+    "top-R": " ",
+    "mid-L": " ",
+    "mid-M": " ",
+    "mid-R": " ",
+    "low-L": " ",
+    "low-M": " ",
+    "low-R": " ",
+}
+
+
+def print_board(board):
+    print(board["top-L"] + "|" + board["top-M"] + "|" + board["top-R"])
+    print("-+-+-")
+    print(board["mid-L"] + "|" + board["mid-M"] + "|" + board["mid-R"])
+    print("-+-+-")
+    print(board["low-L"] + "|" + board["low-M"] + "|" + board["low-R"])
+
+
+def isWinner(bo, le):
+    # Given a board and a player’s letter, this function returns True if that player has won.
+    # using bo instead of board and le instead of letter so we don’t have to type as much.
+    return (
+        (
+            bo["top-L"] == le and bo["top-M"] == le and bo["top-R"] == le
+        )  # across the top
+        or (
+            bo["mid-L"] == le and bo["mid-M"] == le and bo["mid-R"] == le
+        )  # across the middle
+        or (
+            bo["low-L"] == le and bo["low-M"] == le and bo["low-R"] == le
+        )  # across the bottom
+        or (
+            bo["top-L"] == le and bo["mid-L"] == le and bo["low-L"] == le
+        )  # down the left side
+        or (
+            bo["top-M"] == le and bo["mid-M"] == le and bo["low-M"] == le
+        )  # down the middle
+        or (
+            bo["top-R"] == le and bo["mid-R"] == le and bo["low-R"] == le
+        )  # down the right side
+        or (bo["top-L"] == le and bo["mid-M"] == le and bo["low-R"] == le)  # diagonal
+        or (bo["top-R"] == le and bo["mid-M"] == le and bo["low-L"] == le)
+    )  # diagonal
+
+
+print("Welcome to Tic Tac Toe!")
+turn = "X"
+
+for i in range(9):
+    print_board(theBoard)
+
+    print("Turn for " + turn + ". Move on which space? ")
+    move = input()
+    theBoard[move] = turn
+
+    if isWinner(theBoard, turn):
+        print_board(theBoard)
+        print("Hooray! You have won the game!")
+        break
+
+    if turn == "X":
+        turn = "O"
+    else:
+        turn = "X"
