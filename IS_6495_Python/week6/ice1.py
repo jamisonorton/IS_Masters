59a67604e5f2d966b9ea87fc9223ace5afbc861d
Jamison Orton <jorton4@byu.edu>
2026-03-20 10:30:17 -0600

    ignore prettier for ice1?

--- IS_6495_Python/week6/ice1.py
+++ IS_6495_Python/week6/ice1.py
@@ -1,26 +1,27 @@
-# 1. Name function (5 points)
-# Write a function that takes 3 strings - first name, last name and middle initial.
-# Return one string that has the person’s full name.  Make sure that the first letter of each name is capitalized (.title()) and then return the string.
-# Use the .format() string function in the return statement.
-def full_name(first_name, last_name, middle_initial):
-    first_name = first_name.title()
-    last_name = last_name.title()
-    middle_initial = middle_initial.title()
+# # 1. Name function (5 points)
+# # Write a function that takes 3 strings - first name, last name and middle initial.
+# # Return one string that has the person’s full name.  Make sure that the first letter of each name is capitalized (.title()) and then return the string.
+# # Use the .format() string function in the return statement.
+# def full_name(first_name, last_name, middle_initial):
+#     first_name = first_name.title()
+#     last_name = last_name.title()
+#     middle_initial = middle_initial.title()
 
-    return str.format(first_name + " " + middle_initial + " " + last_name)
+#     return "{} {}. {}".format(first_name, middle_initial, last_name)
 
 
-first_name = input("What is your First name? ")
-last_name = input("What is your Last name? ")
-middle_initial = input("What is your Middle initial? ")
+# first_name = input("What is your First name? ")
+# last_name = input("What is your Last name? ")
+# middle_initial = input("What is your Middle initial? ")
 
-print("Your name is:", full_name(first_name, last_name, middle_initial))
+# print("Your name is:", full_name(first_name, last_name, middle_initial))
 
 
 # 2. String function practice (5 points)
 # 1.) Using escape characters, print out the phrase “Welcome to O’Neil’s Boat Rentals!”
+print("Welcome to O'Neil's Boat Rentals!")
 # 2.) Initialize one string variable to the following sentence:
-# “Hello there! How are you? I'm doing fine."
+greeting = "Hello there!\nHow are you?\nI'm doing fine."
 
 # Using the print() function and escape characters, make the output look exactly as below: (Be sure there’s a single quote in the word I’m.
 
@@ -28,6 +29,7 @@ print("Your name is:", full_name(first_name, last_name, middle_initial))
 # Hello there!
 # How are you?
 # I'm doing fine.
+print(greeting)
 # 3.) Initialize a string variable to “hello python” and print it out in all uppercase using a string function.
 # 4.) Write a small program using a ‘while’ loop (while True).
 # 1.) Prompt the user to enter their age.
